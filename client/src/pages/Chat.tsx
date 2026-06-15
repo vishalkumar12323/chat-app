a9be2e8ac5474c173f749164a6942019eb21f298
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-15 16:19:04 +0530

    Renders TypingIndicator between message-list and input

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 import MessageList from '../components/MessageList';
 import MessageInput from '../components/MessageInput';
+import TypingIndicator from '../components/TypingIndicator';
 import UserList from '../components/UserList';
 import useChatStore from '../store/chatStore';
 import useAuthStore from '../store/authStore';
@@ -24,6 +25,7 @@ const Chat = () => {
             <Sidebar />
             <div className="flex-1 flex flex-col min-w-0">
                 <MessageList />
+                <TypingIndicator />
                 <MessageInput />
             </div>
             <UserList />

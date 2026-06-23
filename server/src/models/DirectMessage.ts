ecf4fc4846936cee6df302c23fd63ef709784344
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-23 17:33:51 +0530

    Edit DirectMessage model add file_id as foreignkey.

--- server/src/models/DirectMessage.ts
+++ server/src/models/DirectMessage.ts
@@ -1,14 +1,17 @@
 import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../config/database';
 
+export type MessageType = 'TEXT' | 'IMAGE' | 'VIDEO' | 'DOCUMENT';
 export interface DirectMessageAttributes {
     id: string;
+    type: MessageType;
     content: string;
     sender_id: string;
     recipient_id: string;
+    file_id?: string;
 }
 
-interface DirectMessageCreationAttributes extends Optional<DirectMessageAttributes, 'id'> {}
+interface DirectMessageCreationAttributes extends Optional<DirectMessageAttributes, 'id' | 'content' | 'file_id'> { }
 
 const DirectMessage = sequelize.define<Model<DirectMessageAttributes, DirectMessageCreationAttributes>>('direct_messages', {
     id: {
@@ -16,9 +19,14 @@ const DirectMessage = sequelize.define<Model<DirectMessageAttributes, DirectMess
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
     },
+    type: {
+        type: DataTypes.ENUM('TEXT', 'IMAGE', 'VIDEO', 'DOCUMENT'),
+        allowNull: false,
+        defaultValue: 'TEXT',
+    },
     content: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: true,
     },
     sender_id: {
         type: DataTypes.UUID,
@@ -28,6 +36,10 @@ const DirectMessage = sequelize.define<Model<DirectMessageAttributes, DirectMess
         type: DataTypes.UUID,
         allowNull: false,
     },
+    file_id: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
 }, {
     timestamps: true,
 });

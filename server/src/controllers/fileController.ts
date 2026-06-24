664fe43bc0263c9c1f9af8361f81aeb8e7119924
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-24 14:58:45 +0530

    fe:[media-exchange]=> create files controller handler functions.

---
+++ server/src/controllers/fileController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { File } from "../models";
+
+
+export const uploadFile = async (req: Request, res: Response) => {
+
+};
+
+
+export const getFileById = async (req: Request, res: Response) => {
+
+};
+
+
+export const downloadFile = async (req: Request, res: Response) => {
+
+};
+
+export const deleteFile = async (req: Request, res: Response) => {
+
+};

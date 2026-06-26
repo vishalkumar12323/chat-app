1cb0fac2182552fa76df55df3fb2ae05b8a236ee
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-26 17:20:34 +0530

    fe:[media-exchange]=> Change types and new ENUM feilds to ChannelMessage.ts file.

--- server/src/models/ChannelMessage.ts
+++ server/src/models/ChannelMessage.ts
@@ -1,14 +1,18 @@
 import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../config/database';
 
+export type MessageType = 'TEXT' | 'IMAGE' | 'DOCUMENT';
+
 export interface ChannelMessageAttributes {
     id: string;
+    type: MessageType;
     content: string;
     sender_id: string;
     channel_id: string;
+    file_id?: string;
 }
 
-interface ChannelMessageCreationAttributes extends Optional<ChannelMessageAttributes, 'id'> {}
+interface ChannelMessageCreationAttributes extends Optional<ChannelMessageAttributes, 'id' | 'content' | 'file_id'> {}
 
 const ChannelMessage = sequelize.define<Model<ChannelMessageAttributes, ChannelMessageCreationAttributes>>('channel_messages', {
     id: {
@@ -16,9 +20,14 @@ const ChannelMessage = sequelize.define<Model<ChannelMessageAttributes, ChannelM
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
     },
+    type: {
+        type: DataTypes.ENUM('TEXT', 'IMAGE', 'DOCUMENT'),
+        allowNull: false,
+        defaultValue: 'TEXT',
+    },
     content: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: true,
     },
     sender_id: {
         type: DataTypes.UUID,
@@ -27,6 +36,14 @@ const ChannelMessage = sequelize.define<Model<ChannelMessageAttributes, ChannelM
     channel_id: {
         type: DataTypes.UUID,
         allowNull: false,
+    },
+    file_id: {
+        type: DataTypes.UUID,
+        allowNull: true,
+        references: {
+            model: 'files',
+            key: 'id',
+        },
     },
 }, {
     timestamps: true,
